a07641ca773adc980ee7f84dc8ada35298e2246c
sareljohn <18103999@usc.edu.ph>
2025-06-18 20:57:18 +0800

    Get requests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import express from "express";
+import express, { request, response } from "express";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (request, response) => {
+    response.status(201).send({ msg: "Hello Love!"});
+});
+
+app.get('/api/users', (request, response) => {
+    response.send([
+        {id: 1, username: "jon", displayName: "Jon"}, 
+        {id: 2, username: "siu", displayName: "Siu"},
+        {id: 3, username: "dex", displayName: "Dex"}
+    ]);
+})
+
+app.get('/api/products', (request, response) => {
+    response.send([
+        {id: 1, name: "Steak", price: "99.00"}
+    ]);
+})
+
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
 });
+
